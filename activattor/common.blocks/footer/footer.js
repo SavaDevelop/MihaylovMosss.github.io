f20de69ab9cd4e06e80aea2fedd9ac751383150e
Moisey <Mosha.site>
2019-05-30 20:40:34 +0300

    crash kakashka

--- activattor/common.blocks/footer/footer.js
+++ activattor/common.blocks/footer/footer.js
@@ -1,15 +1,15 @@
 import './footer.sass';
 
 $(document).ready(function(){
-    $('#footer-pokupatelyam .footer_heading').on('click', function() {
-      $('#footer-pokupatelyam .footer_nav').toggleClass('active');
-    });
-  
-    $('#footer-partneram .footer_heading').on('click', function() {
-      $('#footer-partneram .footer_nav').toggleClass('active');
-    });
-  
-    $('#footer-informatsiya .footer_heading').on('click', function() {
-      $('#footer-informatsiya .footer_nav').toggleClass('active');
-    });
+  $('#footer-pokupatelyam .footer_heading').on('click touch', function() {
+    $('#footer-pokupatelyam .footer_nav').toggleClass('active');
+  });
+
+  $('#footer-partneram .footer_heading').on('click touch', function() {
+    $('#footer-partneram .footer_nav').toggleClass('active');
+  });
+
+  $('#footer-informatsiya .footer_heading').on('click touch', function() {
+    $('#footer-informatsiya .footer_nav').toggleClass('active');
+  });
 });
