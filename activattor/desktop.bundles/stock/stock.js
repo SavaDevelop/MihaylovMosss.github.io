896473ab4ef7ec7e9a3cdae122a42807faddc0ec
Savin Anton <business.savin@gmail.com>
2019-06-04 20:34:31 +0300

    page stock & common block take-advantage

--- activattor/desktop.bundles/stock/stock.js
+++ activattor/desktop.bundles/stock/stock.js
@@ -1,1 +1,6 @@
 import '../../common.blocks/base/base.js';
+import './stock.sass';
+import '../../common.blocks/button/button.js';
+import '../../common.blocks/trafarets/trafarets.js';
+import '../../common.blocks/title/title.js';
+import '../../common.blocks/take-advantage/take-advantage.js';
