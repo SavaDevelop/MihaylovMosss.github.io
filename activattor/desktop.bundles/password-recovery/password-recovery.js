5ffa8d4696b5da6bb4ce62a781b2213f4301d53e
Savin Anton <business.savin@gmail.com>
2019-06-05 15:23:18 +0300

    webpack, checkin, passwd

---
+++ activattor/desktop.bundles/password-recovery/password-recovery.js
@@ -0,0 +1,6 @@
+import '../../common.blocks/base/base.js';
+import './password-recovery.sass';
+
+import '../../common.blocks/button/button.js';
+import '../../common.blocks/trafarets/trafarets.js';
+import '../../common.blocks/title/title.js';
