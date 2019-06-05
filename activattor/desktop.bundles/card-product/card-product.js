9f76d1fe38c31e4ec738d31b095b8cb360a4c24c
Moisey <Mosha.site>
2019-06-06 00:53:57 +0300

    fixed video-block and catalog

--- activattor/desktop.bundles/card-product/card-product.js
+++ activattor/desktop.bundles/card-product/card-product.js
@@ -1,9 +1,12 @@
 import '../../common.blocks/base/base.js';
 import './card-product.sass';
-import './sass/_product_gallery.scss';
-import './sass/_single-product__crossell_block.sass';
-import './sass/_single-product__feauters_block.sass';
-import './sass/_single-product__gallery.scss';
-import './sass/_single_product_gallery.scss';
+import './sass/product_gallery.scss';
+import './sass/single-product__properties.sass';
+import './sass/single-product__crossell_block.sass';
+import './sass/single-product__feauters_block.sass';
+import './sass/single-product__gallery.scss';
+import './sass/single_product_gallery.scss';
+import './sass/single-product__testimonials.sass';
+import './sass/single-product__question_form.sass';
 import './js/single-product_script.js';
 import './js/product_gallery_slick.js';
