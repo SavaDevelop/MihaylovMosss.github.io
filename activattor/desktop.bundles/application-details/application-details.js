acba49a6a7c61033da872d6c43414f457c378160
Savin Anton <business.savin@gmail.com>
2019-06-06 20:39:37 +0300

    fix page aplication detail

--- activattor/desktop.bundles/application-details/application-details.js
+++ activattor/desktop.bundles/application-details/application-details.js
@@ -5,7 +5,17 @@ import '../../common.blocks/button/button.js';
 import '../../common.blocks/title/title.js';
 
 $(function(){
-  $(document).on('click rouch', '.app-det__ord-detais', function(){
-    $('.app-det_js-detail, .app-det_js-go-main', document).show(500);
-  })
-})
+
+  $(document).on('click touch', '.app-det__ord-detais', function(){
+
+    $('.app-det_js-detail', document).toggleClass(function() {
+      if ($(this).hasClass('.d-none')) {
+        return 'd-none';
+      } else {
+        return 'd-block';
+      }
+    })
+
+  });
+
+});
