4ce751951549e3aee5cbf1819fcfa96e64219189
Savin Anton <business.savin@gmail.com>
2019-06-06 22:44:46 +0300

    add kasrkas page saite-map, basket

---
+++ activattor/desktop.bundles/basket/basket.js
@@ -0,0 +1,5 @@
+import '../../common.blocks/base/base.js';
+import './basket.sass';
+
+import '../../common.blocks/button/button.js';
+import '../../common.blocks/title/title.js';
---
+++ activattor/desktop.bundles/site-map/site-map.js
@@ -0,0 +1,5 @@
+import '../../common.blocks/base/base.js';
+import './site-map.sass';
+
+import '../../common.blocks/button/button.js';
+import '../../common.blocks/title/title.js';
