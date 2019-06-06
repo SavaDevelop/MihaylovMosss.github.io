0bfeaac11903e54e70b41aaf2b8a15a4da795b97
Savin Anton <business.savin@gmail.com>
2019-06-06 19:52:16 +0300

    add pages: application-accepted, application-details. fix button.sass. Add trafarets

---
+++ activattor/desktop.bundles/application-details/application-details.js
@@ -0,0 +1,11 @@
+import '../../common.blocks/base/base.js';
+import './application-details.sass';
+
+import '../../common.blocks/button/button.js';
+import '../../common.blocks/title/title.js';
+
+$(function(){
+  $(document).on('click rouch', '.app-det__ord-detais', function(){
+    $('.app-det_js-detail, .app-det_js-go-main', document).show(500);
+  })
+})
