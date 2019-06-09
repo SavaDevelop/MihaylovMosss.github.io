ee9b6d0a7d7bbcc7d5c5d483be59d232598c498f
Savin Anton <business.savin@gmail.com>
2019-06-09 20:56:12 +0300

    page

---
+++ activattor/desktop.bundles/compare/compare.js
@@ -0,0 +1,8 @@
+import '../../common.blocks/base/base.js';
+import './compare.sass';
+
+import '../../common.blocks/compare-slider/compare-slider.js';
+import '../../common.blocks/button/button.js';
+import '../../common.blocks/title/title.js';
+import '../../common.blocks/trafarets/trafarets.js';
+import '../../common.blocks/input/input.js';
