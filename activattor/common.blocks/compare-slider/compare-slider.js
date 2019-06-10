45c810ae2ba9f76cae786cc9237c736d490b7f65
Savin Anton <business.savin@gmail.com>
2019-06-10 17:26:19 +0300

    fix compare-slider

--- activattor/common.blocks/compare-slider/compare-slider.js
+++ activattor/common.blocks/compare-slider/compare-slider.js
@@ -60,5 +60,56 @@ $(document).ready(function(){
       }
     ]
   })
-  //*/
+  $('.compare-slider__slider-tab2').slick({
+    rows: 0, // delete empty div
+    dots: false,
+    arrow: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+      {
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          arrow: true
+        }
+      },
+      {
+        breakpoint: 1170,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 992,
+        settings: {
+          centerMode: true,
+          centerPadding: '80px',
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          centerMode: true,
+          centerPadding: '20px',
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  })
 });
