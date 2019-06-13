9bc69a7602ee60652604ded315da8eb9bc82967d
Moisey <Mosha.site>
2019-06-14 02:45:37 +0300

    fix about

--- activattor/desktop.bundles/about/about.js
+++ activattor/desktop.bundles/about/about.js
@@ -1,2 +1,11 @@
 import '../../common.blocks/base/base.js';
 import './about.sass';
+
+jQuery(document).ready(function(){
+    document.getElementsByClassName('yellow-circle')[0].onclick = function () {
+        document.getElementById('movie1').style('display', 'block');
+        document.getElementById('movie1').play();
+        document.getElementById('movie1').setAttribute('controls', 'controls');
+        this.remove();
+    }
+});
