762bd845e551d8d2ca298517a2851a6302436594
Savin Anton <business.savin@gmail.com>
2019-06-14 00:11:40 +0300

    fix compare-slider

--- activattor/common.blocks/compare-slider/compare-slider.js
+++ activattor/common.blocks/compare-slider/compare-slider.js
@@ -3,12 +3,14 @@ import '../base/sass/bootstrap-slider.scss';
 
 $(document).ready(function(){
 
+  //Показываем блоки свойств помеченые специальным классом
   $('.compare-slider__switcher').on('click', function () {
     $('.compare-slider__switcher').toggleClass('compare-slider__switcher-on'); //меняем положение переключателя
     $('.compare-slider__switcher-off').toggleClass('compare-slider__switcher-on'); //меняем положение переключателя
     $('.compare__item:not(.compare__item_bg_blue)').toggleClass('d-none'); //прячем все блоки, которе не имею класс .compare__item_bg_blu т.е. блоки тольчко отличия
   });
 
+  //инициализируем слайдер, делаем 1 на все, т.к. их может быть более 2х
   $('.compare-slider__slider').slick({
     rows: 0, // delete empty div
     dots: false,
@@ -52,71 +54,27 @@ $(document).ready(function(){
         breakpoint: 480,
         settings: {
           centerMode: true,
-          centerPadding: '20px',
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-
-  $('.compare-slider__slider-tab2').slick({
-    rows: 0, // delete empty div
-    dots: false,
-    arrow: true,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-      {
-        breakpoint: 1280,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          arrow: true
-        }
-      },
-      {
-        breakpoint: 1170,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 992,
-        settings: {
-          centerMode: true,
-          centerPadding: '80px',
+          centerPadding: '10px',
           slidesToShow: 2,
           slidesToScroll: 1
         }
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          centerMode: true,
-          centerPadding: '20px',
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
       }
     ]
   });
 
+  //при нажатии на вкладку, делаем переинициализацию слайдера
   $('ul.nav li a', document).on('click touch', function(){
-    //обновляем слайдер для его правильного отображения
-    $('.compare-slider__slider, .compare-slider__slider-tab2', document).slick('refresh');
+    $('.compare-slider__slider').css({'opacity': 0}); // прячем слайдер, чтобы он не скакал
+    $('.compare-slider').css({'height': '261px'}); // даем временно высоту, иначе скачет размер
+    if($('.nav-link').is('.active')){
+      setTimeout(function(){
+        $('.compare-slider__slider').slick('refresh'); //обновляем слайдер, иначе не строится ширина
+        $('.compare-slider__slider').css({'opacity': 1}); // показываем слайдер
+        $('.compare-slider').css({'height': 'auto'}); // возвращяет высоту
+      },150); //критическое время 150, меньше нельзя
+    }
   })
+
 
   $('.compare-slider__item-close', document).on('click touch', function(){
     //Удаляем карточки при нажатии крестика пользователем
