e4d5f31877f6c20901636a67681421cbe9579fe0
Savin Anton <business.savin@gmail.com>
2019-06-18 16:54:58 +0300

    card-production

--- activattor/desktop.bundles/card-product/card-product.js
+++ activattor/desktop.bundles/card-product/card-product.js
@@ -1,13 +1,9 @@
 import '../../common.blocks/base/base.js';
 import '../../common.blocks/card-product/card-product.js';
 import './card-product.sass';
-import './sass/product_gallery.scss';
 import './sass/single-product__properties.sass';
 import './sass/single-product__crossell_block.sass';
 import './sass/single-product__feauters_block.sass';
-import './sass/single-product__gallery.scss';
-import './sass/single_product_gallery.scss';
 import './sass/single-product__testimonials.sass';
 import './sass/single-product__question_form.sass';
 import './js/single-product_script.js';
-import './js/product_gallery_slick.js';
